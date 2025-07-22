d91b7943d17bdaa79cded27de2b2e4cc2a5caecf
NamiMIURA <735namiko@gmail.com>
2025-07-22 15:00:50 +0900

    feat: LINE初期設定できた

--- src/pages/api/line/webhook.ts
+++ src/pages/api/line/webhook.ts
@@ -1,1 +1,16 @@
 // LINEからのWebhook受信
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// Webhookを受け取る関数。APIのエンドポイント。/api/line/webhookにPOSTリクエストが来たら、この関数が呼ばれる。
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    console.log("Received LINE webhook event:", req.body);
+    // ここにLINEメッセージ処理を書く
+    res.status(200).json({ message: "OK" });
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+}
