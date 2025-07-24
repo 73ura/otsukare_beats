d1fc57c7be3e7d08cf3a59f91a9d27009b4f4508
kyoko <kumakyon9s@gmail.com>
2025-07-24 22:41:24 +0900

    chore: ログ出力用ライブラリwinstonを導入

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,1 +1,17 @@
 // 共通ログ処理
+
+import { createLogger, format, transports } from "winston";
+
+export const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.File({ filename: "logs/error.log", level: "error" }), // エラーログ
+    new transports.File({ filename: "logs/combined.log" }), // 全てのログ
+  ],
+});
