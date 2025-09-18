060bc49b12f614df46604116ea412df269a2fdce
NamiMIURA <735namiko@gmail.com>
2025-09-18 21:49:31 +0900

    Prismaクライアントでprepared statements無効化を強制設定

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,11 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL + '?preparedStatements=false&pgbouncer=true'
+    }
+  }
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
